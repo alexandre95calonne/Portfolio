c52a232a6591827c527d11812d047f25496667c4
Alexandre Calonne <alexandre.calonne@epitech.digital>
2023-05-29 21:01:06 +0200

    deployment

---
+++ src/Experience.js
@@ -0,0 +1,63 @@
+import {
+  Text,
+  Html,
+  ContactShadows,
+  PresentationControls,
+  Float,
+  Environment,
+  useGLTF,
+} from "@react-three/drei";
+
+export default function Experience() {
+  const computer = useGLTF(
+    "https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf"
+  );
+  console.log(computer);
+
+  return (
+    <>
+      <Environment preset="city" />
+
+      <color args={["#262423"]} attach="background" />
+
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float rotationIntensity={0.4}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={"#DF402D"}
+            rotation={[-0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
+
+          <primitive
+            object={computer.scene}
+            position-y={-1.35}
+            position-x={0.6}
+            rotation-y={-0.2}
+          >
+            <Html
+              transform
+              wrapperClass="htmlScreen"
+              distanceFactor={1.1}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://alexandre95calonne.github.io/portfolio-html/" />
+            </Html>
+          </primitive>
+        </Float>
+      </PresentationControls>
+
+      <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
+    </>
+  );
+}

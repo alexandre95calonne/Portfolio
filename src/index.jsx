acef7cd6f99fa33d9b5d7e9e13fb7ee9f3a8432c
alexandrecalonne <alexandre.calonne@epitech.eu>
2024-08-06 19:50:05 +0200

    [FEAT] : new versio,n

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,39 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
+import { useEffect, useState } from "react";
+
+function ResponsiveCanvas() {
+  const [cameraPosition, setCameraPosition] = useState([-2.5, 1.7, 4]);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 768) {
+        setCameraPosition([-5, 3.4, 8]);
+      } else {
+        setCameraPosition([-2.5, 1.7, 4]);
+      }
+    }
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <Canvas
+      camera={{
+        fov: 42,
+        near: 0.1,
+        far: 2000,
+        position: cameraPosition,
+      }}
+    >
+      <Experience />
+    </Canvas>
+  );
+}
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(
-  <Canvas
-    camera={{
-      fov: 42,
-      near: 0.1,
-      far: 2000,
-      position: [-2.5, 1.7, 4],
-    }}
-  >
-    <Experience />
-  </Canvas>
-);
+root.render(<ResponsiveCanvas />);

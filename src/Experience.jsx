8210999e41ee33c3177644a50e4a36f9013eb207
alexandrecalonne <alexandre.calonne@epitech.eu>
2024-08-06 16:28:25 +0200

    [FEAT] : change camera and iframe

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,12 +1,12 @@
 import {
-    Text,
-    Html,
-    ContactShadows,
-    PresentationControls,
-    Float,
-    Environment,
-    useGLTF,
-  } from "@react-three/drei";
+  Text,
+  Html,
+  ContactShadows,
+  PresentationControls,
+  Float,
+  Environment,
+  useGLTF,
+} from "@react-three/drei";
 
 export default function Experience() {
   const computer = useGLTF(

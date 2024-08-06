8a2735b5e4c983f8a0c990e1d8eeb7c5bb469ae4
alexandrecalonne <alexandre.calonne@epitech.eu>
2024-08-06 19:54:37 +0200

    [FIX] : removed commentary

--- src/index.jsx
+++ src/index.jsx
@@ -10,21 +10,17 @@ function ResponsiveCanvas() {
   useEffect(() => {
     function handleResize() {
       if (window.innerWidth < 768) {
-        // Zoom out more for mobile devices
-        setCameraPosition([-5, 3.4, 8]); // Increase these values to zoom out more
+        setCameraPosition([-5, 3.4, 8]);
       } else {
-        // Default position for larger screens
         setCameraPosition([-2.5, 1.7, 4]);
       }
     }
 
-    // Set initial position
     handleResize();
 
-    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
     window.addEventListener("resize", handleResize);
 
-    // Clean up
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
